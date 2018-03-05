f5755d2587de36035c2416b4165f6eafc459e01e
Alistair1231 <alistair1231@gmail.com>
2018-03-05 23:49:06 +0100

    new url

--- kittensUserscript.js
+++ kittensUserscript.js
@@ -8,7 +8,7 @@
 // @grant        none
 // ==/UserScript==
 
-$('<script src="https://raw.githack.com/Alistair1231/Kittens-Game-automation-and-tools/new/kittens.js"></script>').insertBefore("div[id*='gamePageContainer']");
+$('<script src="https://raw.githack.com/Alistair1231/Kittens-Game-automation-and-tools/master/kittens.js"></script>').insertBefore("div[id*='gamePageContainer']");
 
 $( "<a href='#' onclick='bot.trade(1)'>Trade</a><span> </span><a href='#' onclick='bot.trade(10)'>Off</a><span> | </span>" ).insertBefore("a[onclick*='gamePage.ui.hideChat();']");
 $( "<a href='#' onclick='bot.start()'>Start</a><span> </span><a href='#' onclick='bot.start(1)'>1</a><span> </span><a href='#' onclick='bot.start(2)'>2</a><span> </span><a href='#' onclick='bot.start(3)'>3</a><span> </span><a href='#' onclick='bot.start(4)'>4</a><span> </span><a href='#' onclick='bot.start(10,20,30)'>Off</a><span> | </span>" ).insertBefore("a[onclick*='bot.trade(1)']");
