f4223d5c9bcd5f80561560e7e4b1efa2b1234f79
Alistair1231 <alistair1231@gmail.com>
2018-03-08 21:21:26 +0100

    reworked functions, autosave, unlocked
    reworked how features get activated
    set autosave delay to 50
    addad unlocked check

--- kittens.js
+++ kittens.js
@@ -1,3 +1,4 @@
+gamePage.autosaveFrequency = 50;
 var bot = (
     function() 
     {
@@ -22,88 +23,56 @@ var bot = (
             trade:function()
             {
             	trade=setInterval(function(){            	
-                    if(gamePage.resPool.get('uranium').value < 5)
+                    if(gamePage.resPool.get('uranium').value < 5 && gamePage.diplomacy.get('dragons').unlocked)
                         gamePage.diplomacy.tradeAll(gamePage.diplomacy.get('dragons'));
-                    else
+                    else if(gamePage.diplomacy.get('zebras').unlocked)
 	            		gamePage.diplomacy.tradeAll(gamePage.diplomacy.get('zebras'));
                     },10000);
             },
             start:function()
             {
-                console.log("'compedium','manuscript','blueprint'");
                 if(arguments.length>=1)
                 {
                     var length = 15000;
-                    for (var i = arguments.length - 1; i >= 0; i--) {
+                    for (var i = arguments.length - 1; i >= 0; i--) 
+                    {
                         if(arguments[i]==30)
                         	clearInterval(compedium);
-                        if(arguments[i]==3)
+                        if(arguments[i]==3 && gamePage.workshop.getCraft('compendium').unlocked)
                             compedium = setInterval(function(){gamePage.craftAll('compedium');},length);
                         
                         if(arguments[i]==20)
                         	clearInterval(manuscript);
-                        if(arguments[i]==2)
+                        if(arguments[i]==2 && gamePage.workshop.getCraft('manuscript').unlocked)
                             manuscript = setInterval(function(){gamePage.craftAll('manuscript');},length);
 
                         if(arguments[i]==10)
                         	clearInterval(parchment);
-                        if(arguments[i]==1)
+                        if(arguments[i]==1 && gamePage.workshop.getCraft('parchment').unlocked)
                             parchment = setInterval(function(){gamePage.craftAll('parchment');},length);
                 		
                         
                         if(arguments[i]==40)
                         	clearInterval(blueprint);
-                        if(arguments[i]==4)
+                        if(arguments[i]==4 && gamePage.workshop.getCraft('blueprint').unlocked)
                             blueprint = setInterval(function(){gamePage.craftAll('blueprint');},length);
-                		
-                		if(arguments[i]=='autoHunt')
-                			clearInterval(autoHunt);    	
-              			
-
-		                // clearInterval(starClick);
-		                // clearInterval(autoCraft);
-		                // clearInterval(autoPray);
-		                // clearInterval(autoObserve);
 
                     }
                 }
-
-                
-                starClick = setInterval(function() 
-                {
-                    $("#gameLog").find("input").click();
-                }, 2 * 1000);
-
-                autoObserve = setInterval(function() 
-                {
-                    $("input[id*='observeBtn']").click();
-                },10*1000);
-
-
+            },       
+            hunt:function()
+            {
                 autoHunt = setInterval(function() 
                 {
                     var catpower = gamePage.resPool.get('manpower');
                     if (catpower.value / catpower.maxValue > 0.95) 
                     {
                         $("a:contains('Send hunters')").click();
-                        if (gamePage.workshop.getCraft('parchment').unlocked) 
-                        {
-                            //gamePage.craftAll('parchment');
-                        }
-                        if (gamePage.workshop.getCraft('manuscript').unlocked) 
-                        {
-                            //gamePage.craftAll('manuscript');
-                        }
-                        if (gamePage.workshop.getCraft('compedium').unlocked) 
-                        {
-                            //gamePage.craftAll('compedium');
-                        }
-                        //if (gamePage.workshop.getCraft('blueprint').unlocked)  { gamePage.craftAll('blueprint');  }
-
                     }
                 }, 5 * 1000);
-
-
+            },
+            craft:function()
+            {                
                 autoCraft = setInterval(function() 
                 {
                     var resources = 
@@ -128,22 +97,21 @@ var bot = (
                         }
                     }
                 }, 2 * 1000);
-
+            },
+            pray:function()
+            {
                 autoPray = setInterval(function() 
                 {
-                    
                     if (gamePage.resPool.get('faith').value / gamePage.resPool.get('faith').maxValue > 0.95) 
                     {
-                 		this.game.religion.praise();
+                        this.game.religion.praise();
                     }
 
                 }, 10 * 1000);
-
-
-            }
-
+            },
         };
-})();
+    }       
+)();
 
 
 function copyToClipboard(element) {
@@ -153,8 +121,3 @@ function copyToClipboard(element) {
   document.execCommand("copy");
   $temp.remove();
 }
-
-save = setInterval(function()
-{
-    gamePage.saveUI();
-}, 1000*15);
