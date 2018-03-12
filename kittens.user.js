0722495f0cbf996ac4d32fdff622c1fdcc098518
Alistair1231 <alistair1231@gmail.com>
2018-03-12 19:28:13 +0100

    string

--- kittens.user.js
+++ kittens.user.js
@@ -19,16 +19,16 @@ var ln1='';
 var ln2='';
 var ln3='';
 
-script+='<script src="https://rawcdn.githack.com/Alistair1231/Kittens-Game-automation-and-tools/e5c32c6/kittens.js"></script>'+
-	'<script>let bot = new kittenBot();</script>';
-ln1+="<a href='#' onclick='bot.trade(1)'>Trade</a><span> | </span>";
-ln2+="<a href='#' onclick='bot.start(1)'>1</a><span> </span><a href='#' onclick='bot.start(2)'>2</a><span> </span>"+
-	"<a href='#' onclick='bot.start(3)'>3</a><span> </span><a href='#' onclick='bot.start(4)'>4</a><span> | </span>"+
-	"<span> </span><a href='#' onclick='bot.pray()'>Pray</a><span> | </span><span> </span>"+
-	"<a href='#' onclick='bot.craft()'>Craft</a><span> | </span><span> </span>"+
-	"<a href='#' onclick='bot.hunt()'>Hunt</a><span> | </span>";
-ln3+="<a href='#' onclick='bot.export()'>Export</a><span> </span><a href='#' onclick='bot.import()'>Import</a>"+
-	"<span> | </span>";
+var script=`<script src="https://rawcdn.githack.com/Alistair1231/Kittens-Game-automation-and-tools/e5c32c6/kittens.js"></script>
+	<script>let bot = new kittenBot();</script>`;
+var ln1=`<a href='#' onclick='bot.trade(1)'>Trade</a><span> | </span>`;
+var ln2=`<a href='#' onclick='bot.start(1)'>1</a><span> </span><a href='#' onclick='bot.start(2)'>2</a><span> </span>
+	<a href='#' onclick='bot.start(3)'>3</a><span> </span><a href='#' onclick='bot.start(4)'>4</a><span> | </span>
+	<span> </span><a href='#' onclick='bot.pray()'>Pray</a><span> | </span><span> </span>
+	<a href='#' onclick='bot.craft()'>Craft</a><span> | </span><span> </span>
+	<a href='#' onclick='bot.hunt()'>Hunt</a><span> | </span>`;
+var ln3=`<a href='#' onclick='bot.export()'>Export</a><span> </span><a href='#' onclick='bot.import()'>Import</a>
+	<span> | </span>`;
 
 $(script).insertBefore("div[id*='gamePageContainer']");
 $(ln1).insertBefore("a[onclick*='gamePage.ui.hideChat();']");
