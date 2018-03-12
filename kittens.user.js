99f61d2466ebbd23f3f78235905fb3376e946e68
Alistair1231 <alistair1231@gmail.com>
2018-03-12 18:17:57 +0100

    link, version

--- kittens.user.js
+++ kittens.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Kittens Game Automation
 // @namespace    https://github.com/Alistair1231/Kittens-Game-automation-and-tools
-// @version      0.2.4
-// @description  Auto craft resources when full, auto pray, auto hunt, simple save export/import, ...
+// @version      0.2.5
+// @description  Auto craft resources when full, auto pray, auto hunt, auto trade, fast autosave, simple save export/import, ...
 // @author       Alistair1231
 // @match        http*://bloodrizer.ru/games/kittens/*
 // @grant        none
@@ -15,7 +15,7 @@
 // ==/OpenUserJS==
 
 
-$('<script src="https://rawcdn.githack.com/Alistair1231/Kittens-Game-automation-and-tools/854265a/kittens.js"></script>').insertBefore("div[id*='gamePageContainer']");
+$('<script src="https://rawcdn.githack.com/Alistair1231/Kittens-Game-automation-and-tools/f992dc8/kittens.js"></script>').insertBefore("div[id*='gamePageContainer']");
 
 $("<a href='#' onclick='bot.trade(1)'>Trade</a><span> | </span>").insertBefore("a[onclick*='gamePage.ui.hideChat();']");
 $("<a href='#' onclick='bot.start(1)'>1</a><span> </span><a href='#' onclick='bot.start(2)'>2</a><span> </span><a href='#' onclick='bot.start(3)'>3</a><span> </span><a href='#' onclick='bot.start(4)'>4</a><span> | </span><span> </span><a href='#' onclick='bot.pray()'>Pray</a><span> | </span><span> </span><a href='#' onclick='bot.craft()'>Craft</a><span> | </span><span> </span><a href='#' onclick='bot.hunt()'>Hunt</a><span> | </span>").insertBefore("a[onclick*='bot.trade(1)']");
