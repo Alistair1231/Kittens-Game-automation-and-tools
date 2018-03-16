d1b869d9a9d5d3fa2ddd1015098e58f1e4cf1607
Alistair1231 <alistair1231@gmail.com>
2018-03-16 13:20:32 +0100

    Finally completly reworked UI!

--- kittens.user.js
+++ kittens.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kittens Game Automation
 // @namespace    https://github.com/Alistair1231/Kittens-Game-automation-and-tools
-// @version      0.3.0
+// @version      1.0.0
 // @description  Auto craft resources when full, auto pray, auto hunt, auto trade, fast autosave, simple save export/import, ...
 // @author       Alistair1231
 // @match        http*://bloodrizer.ru/games/kittens/*
@@ -15,23 +15,66 @@
 // @author Alistair1231
 // ==/OpenUserJS==
 
-var script='<script src="https://rawcdn.githack.com/Alistair1231/Kittens-Game-automation-and-tools/e3ab5d3/kittens.js"></script>';
+var script='<script src="https://rawcdn.githack.com/Alistair1231/Kittens-Game-automation-and-tools/40e725b/kittens.js"></script>';
 var script2='<script>let bot = new kittenBot();'+
 	'bot.init();'+
 	'bot.export();</script>';
-var ln1="<a href='#' onclick='bot.trade(1)'>Trade</a><span> | </span>";
-var ln2="<a href='#' onclick='bot.start(1)'>1</a><span> </span><a href='#' onclick='bot.start(2)'>2</a><span> </span>"+
-	"<a href='#' onclick='bot.start(3)'>3</a><span> </span><a href='#' onclick='bot.start(4)'>4</a><span> | </span>"+
-	"<span> </span><a href='#' onclick='bot.pray()'>Pray</a><span> | </span><span> </span>"+
-	"<a href='#' onclick='bot.craft()'>Craft</a><span> | </span><span> </span>"+
-	"<a href='#' onclick='bot.hunt()'>Hunt</a><span> | </span>";
-var ln3="<a href='#' onclick='bot.exportSave()'>Export</a><span> </span><a href='#' onclick='bot.importSave()'>Import</a>"+
-	"<span> </span><a href='#' onclick='bot.stop()'>Stop</a><span> | </span>";
+var ln1="<style>"+
+			".tableBox {"+
+			"display: block !important;}"+
+		"</style>"+
+		"<br>"+
+		"<form id='tableBot'>"+
+			"<table>"+
+	    	"<tr>"+
+		    	"<td>"+
+	    			"<input type='checkbox' class='tableBox' name='1' value='1' checked>1</input>"+
+		    	"</td>"+
+		    	"<td>"+
+					"<input type='checkbox' class='tableBox'  name='pray' value='pray' checked>Pray</input>"+
+		    	"</td>"+
+		    	"<td>"+
+					"<a href='#' onclick='bot.run()'>Run</a>"+
+		    	"</td>"+
+	    	"</tr>"+
+	    	"<tr>"+
+	    		"<td>"+
+	    			"<input type='checkbox' class='tableBox'  name='2' value='2' checked>2</input>"+
+	    		"</td>"+
+		    	"<td>"+
+					"<input type='checkbox' class='tableBox'  name='hunt' value='hunt' checked>Hunt</input><span> </span>"+
+		    	"</td>"+
+		    	"<td>"+
+					"<a href='#' onclick='bot.stop()'>Stop</a>"+
+		    	"</td>"+
+	    	"</tr>"+
+	    	"<tr>"+
+	    		"<td>"+
+	    			"<input type='checkbox' class='tableBox' name='3' value='3'>3</input>"+
+	    		"</td>"+
+		    	"<td>"+
+					"<input type='checkbox' class='tableBox' name='craft' value='craft' checked>Craft</input><span> </span>"+
+		    	"</td>"+
+		    	"<td>"+
+					"<a href='#' onclick='bot.export()'>Export</a>"+
+		    	"</td>"+
+	    	"</tr>"+
+	    	"<tr>"+
+	    		"<td>"+
+	    			"<input type='checkbox' class='tableBox' name='4' value='4'>4</input>"+
+	    		"</td>"+
+		    	"<td>"+
+					"<span> </span>"+
+		    	"</td>"+
+		    	"<td>"+
+					"<a href='#' onclick='bot.import()'>Import</a>"+
+		    	"</td>"+
+	    	"</tr>"+
+	    	"</table>"+
+	    "</form>";
 
 $(script).insertBefore("div[id*='gamePageContainer']");
 setTimeout(function(){
 	$(script2).insertBefore("a[onclick*='gamePage.ui.hideChat();']");
 	},2000);
 $(ln1).insertBefore("a[onclick*='gamePage.ui.hideChat();']");
-$(ln2).insertBefore("a[onclick*='bot.trade(1)']");
-$(ln3).insertBefore("a[onclick*='bot.start(1)']");
